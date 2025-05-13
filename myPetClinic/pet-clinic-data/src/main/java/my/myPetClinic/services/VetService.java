7455162cf8db239a780387130195484126323cbc
Galina Zbitneva <galinaknurenko@gmail.com>
2025-05-13 12:43:42 +0300

    Creating Services. Closes #5

---
+++ myPetClinic/pet-clinic-data/src/main/java/my/myPetClinic/services/VetService.java
@@ -0,0 +1,11 @@
+package my.myPetClinic.services;
+
+import my.myPetClinic.model.Vet;
+
+import java.util.Set;
+
+public interface VetService {
+    Vet findById(Long id);
+    Vet save(Vet vet);
+    Set<Vet> findAll();
+}
