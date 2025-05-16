6e8dd0457073b8e3c221d75eca8371a895f1619d
Galina Zbitneva <galinaknurenko@gmail.com>
2025-05-16 14:33:04 +0300

    Refactoring services ti common base interface. Closes #11

--- myPetClinic/pet-clinic-data/src/main/java/my/myPetClinic/services/PetService.java
+++ myPetClinic/pet-clinic-data/src/main/java/my/myPetClinic/services/PetService.java
@@ -2,10 +2,8 @@ package my.myPetClinic.services;
 
 import my.myPetClinic.model.Pet;
 
-import java.util.Set;
 
-public interface PetService {
-    Pet findById(Long id);
-    Pet save(Pet pet);
-    Set<Pet> findAll();
+
+public interface PetService extends CrudService<Pet, Long> {
+
 }
--- myPetClinic/pet-clinic-data/src/main/java/my/myPetClinic/services/VetService.java
+++ myPetClinic/pet-clinic-data/src/main/java/my/myPetClinic/services/VetService.java
@@ -2,10 +2,8 @@ package my.myPetClinic.services;
 
 import my.myPetClinic.model.Vet;
 
-import java.util.Set;
 
-public interface VetService {
-    Vet findById(Long id);
-    Vet save(Vet vet);
-    Set<Vet> findAll();
+
+public interface VetService extends CrudService<Vet, Long>{
+
 }
