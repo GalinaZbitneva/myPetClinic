f1113c4a764fa58758a99573ebdbe6a9f2e657b7
Galina Zbitneva <galinaknurenko@gmail.com>
2025-06-23 16:13:30 +0300

    added specialities entity. Closes #28

--- myPetClinic/pet-clinic-data/src/main/java/my/myPetClinic/model/Vet.java
+++ myPetClinic/pet-clinic-data/src/main/java/my/myPetClinic/model/Vet.java
@@ -1,4 +1,17 @@
 package my.myPetClinic.model;
 
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
 public class Vet extends Person{
+    private Set<Speciality> specialities;
+
+    public Set<Speciality> getSpecialities() {
+        return specialities;
+    }
+
+    public void setSpecialities(Set<Speciality> specialities) {
+        this.specialities = specialities;
+    }
 }
