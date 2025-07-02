3055c38e795c0daec8e96a8cc59b7da7a7f7f14b
Galina Zbitneva <galinaknurenko@gmail.com>
2025-07-02 17:31:24 +0300

    Updated base entity with JPA annotations . Closes #36

--- myPetClinic/pet-clinic-web/src/main/java/my/myPetClinic/controllers/OwnerController.java
+++ myPetClinic/pet-clinic-web/src/main/java/my/myPetClinic/controllers/OwnerController.java
@@ -16,14 +16,14 @@ public class OwnerController {
     }
 
 
-    @RequestMapping({"","/","/index","/index.html"})
+    @RequestMapping({"","/","/index","/index.html", "/find"})
     public String listOwners(Model model){
         model.addAttribute("owners", ownerService.findAll());
         return "owners/index";
     }
 
-    @RequestMapping({"/find"})
-    public String findOwners(){
-        return "notimplemented";
-    }
+   // @RequestMapping({"/find"})
+    //public String findOwners(){
+      //  return "notimplemented";
+  //  }
 }
