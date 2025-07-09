a0cd0537fe888893a8ab3e50e1ec1c324970d1e7
Galina Zbitneva <galinaknurenko@gmail.com>
2025-07-09 12:55:34 +0300

    Updated vet entity with JPA annotations . Closes #38

--- myPetClinic/pet-clinic-data/src/main/java/my/myPetClinic/model/Vet.java
+++ myPetClinic/pet-clinic-data/src/main/java/my/myPetClinic/model/Vet.java
@@ -1,11 +1,22 @@
 package my.myPetClinic.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
 import org.springframework.stereotype.Service;
 
 import java.util.HashSet;
 import java.util.Set;
 
+@Entity
+@Table(name = "vets")
 public class Vet extends Person{
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "vet_specialties", joinColumns = @JoinColumn(name = "vet_id"), inverseJoinColumns = @JoinColumn(name = "speciality_id"))
     private Set<Speciality> specialities = new HashSet<>();
 
     public Set<Speciality> getSpecialities() {
