7116ce95cdf7b4c481ea9666f69f8dd32dba9e76
Galina Zbitneva <galinaknurenko@gmail.com>
2025-11-20 13:01:59 +0300

    writing tests for OwnerController, added html files,

--- myPetClinic/pet-clinic-web/src/test/java/my/myPetClinic/controllers/OwnerControllerTest.java
+++ myPetClinic/pet-clinic-web/src/test/java/my/myPetClinic/controllers/OwnerControllerTest.java
@@ -7,69 +7,75 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Spy;
+import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MockMvcBuilder;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
-import static org.hamcrest.Matchers.hasSize;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
-
 import java.util.HashSet;
 import java.util.Set;
 
+import static net.bytebuddy.matcher.ElementMatchers.is;
+import static org.hamcrest.beans.HasProperty.hasProperty;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-@ExtendWith(MockitoExtension.class)
+
 class OwnerControllerTest {
-    @InjectMocks
-    OwnerController ownerController;
 
     @Mock
     OwnerService ownerService;
+
+@InjectMocks
+    OwnerController controller;
 
     Set<Owner> owners;
 
     MockMvc mockMvc;
 
-   @BeforeEach
+    @BeforeEach
     void setUp() {
         owners = new HashSet<>();
-        owners.add(Owner.builder().id(1L).build());
-        owners.add(Owner.builder().id(2L).build());
+        Owner owner1 = new Owner();
+        owner1.setId(1L);
+        Owner owner2 = new Owner();
+        owner2.setId(2L);
+        owners.add(owner1);
+        owners.add(owner2);
 
-        mockMvc = MockMvcBuilders
-                .standaloneSetup(ownerController)
-                .build();
+MockitoAnnotations.initMocks(this);
 
+        mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
     }
 
     @Test
     void listOwners() throws Exception {
-
         when(ownerService.findAll()).thenReturn(owners);
-//("/owners") как в 8 строчке контроллера @RequestMapping("/owners")
-        //можно проверить другой html путь, например "/owners/index"
-        //"owners/index" - это наш html файл из папки template
+
         mockMvc.perform(get("/owners"))
-                .andExpect(status().is(200))
+                .andExpect(status().isOk())
                 .andExpect(view().name("owners/index"))
-                .andExpect(model().attribute("owners",hasSize(2)));
+                .andExpect(model().attributeExists("owners"));
     }
 
     @Test
-    void findOwners() throws Exception {
+    void showOwner() throws Exception {
+        Owner owner = new Owner();
+        owner.setId(3L);
 
-        when(ownerService.findAll()).thenReturn(owners);
+        when(ownerService.findById(anyLong())).thenReturn(owner);
 
-       mockMvc.perform(get("/owners/find"))
-               .andExpect(status().isOk())
-               .andExpect(view().name("owners/index"))
-               .andExpect(model().attribute("owners",hasSize(2)));
+        mockMvc.perform(get("/owners/1234"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("owners/ownerDetails"))
+                .andExpect(model().attributeExists("owner"));
     }
 }
