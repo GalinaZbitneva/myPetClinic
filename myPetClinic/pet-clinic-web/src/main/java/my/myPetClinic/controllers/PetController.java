3acc00549245665b7bc8f827362b256c208092c3
Galina Zbitneva <galinaknurenko@gmail.com>
2025-12-02 13:01:56 +0300

    add a formatter for PetType and method toString to parse html file a

--- myPetClinic/pet-clinic-web/src/main/java/my/myPetClinic/controllers/PetController.java
+++ myPetClinic/pet-clinic-web/src/main/java/my/myPetClinic/controllers/PetController.java
@@ -58,6 +58,7 @@ public class PetController {
     public String initCreationForm(Owner owner, Model model){
         Pet pet = new Pet();
         owner.getPets().add(pet);
+        pet.setOwner(owner);
         model.addAttribute("pet", pet);
         return VIEWS_PETS_CREATE_OR_UPDATE_FORM;
     }
@@ -92,6 +93,7 @@ public class PetController {
             return VIEWS_PETS_CREATE_OR_UPDATE_FORM;
         } else {
             owner.getPets().add(pet);
+            pet.setOwner(owner);
             petService.save(pet);
             return "redirect:/owners/" + owner.getId();
         }
